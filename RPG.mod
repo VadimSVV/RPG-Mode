990a710c6e01fdb5c0c143c0b5f60fef65c180f1
Veydem <51034142+Alfabet-Translation@users.noreply.github.com>
2021-03-10 22:36:06 +0300

    Update

---
+++ RPG.mod
@@ -0,0 +1,10 @@
+tags={
+	"Graphics"
+	"Gameplay"
+	"Military"
+}
+name="RPG Mode"
+picture="RPG.jpg"
+supported_version="1.10.*"
+path="mod/RPG/"
+remote_file_id="1429338237"
---
+++ RPG/common/defines/Rpg_defines.lua
@@ -0,0 +1,23 @@
+NDefines.NMilitary.TRAINING_MAX_LEVEL = 20
+NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 10
+NDefines.NMilitary.UNIT_EXP_LEVELS = {0.02,	0.04, 0.06, 0.08, 0.10, 0.12, 0.14, 0.16, 0.18, 0.20, 0.22,	0.24, 0.26, 0.28, 0.30, 0.32, 0.34, 0.36, 0.38, 0.40, 0.42, 0.44, 0.46, 0.48, 0.50, 0.52,	0.54, 0.56, 0.58, 0.60, 0.62, 0.64, 0.66, 0.68, 0.70, 0.72, 0.74, 0.76, 0.78, 0.80, 0.82, 0.84, 0.86, 0.88, 0.90, 0.92, 0.94, 0.96, 0.98, 0.99, 0.999}		
+NDefines.NMilitary.EXPERIENCE_COMBAT_FACTOR = 0.05
+NDefines.NMilitary.ARMY_EXP_BASE_LEVEL = 10
+NDefines.NMilitary.EXPERIENCE_LOSS_FACTOR = 0.2
+NDefines.NMilitary.EQUIPMENT_COMBAT_LOSS_FACTOR = 0.6
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 5000
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 5000
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 5000
+NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 48
+NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 0
+NDefines.NMilitary.UNIT_LEADER_INITIAL_TRAIT_SLOT = { 				-- trait slot for 0 level leader
+		1.0, -- field marshal
+		1.0, -- corps commander
+		1.0  -- navy general
+	} 
+NDefines.NMilitary.UNIT_LEADER_TRAIT_SLOT_PER_LEVEL = { 			-- num extra traits on each level
+		4.0, -- field marshal
+		4.0, -- corps commander
+		4.0  -- navy general
+	}
+NDefines.NCountry.BASE_MAX_COMMAND_POWER = 300.0					-- base value for maximum command power
